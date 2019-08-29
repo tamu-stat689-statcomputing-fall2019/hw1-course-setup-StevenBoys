19fdf2fad6808a650fee198479631c7f6bf11d7a
Bowen Lei <bowen.lei@stat.tamu.edu>
2019-08-29 17:22:06 -0500

    Complete the calculateMSE function.

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -25,6 +25,8 @@ calculateBeta <- function(X, Y){
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
+  # calculate MSE between the true beta and the one estimated
+  MSE <- as.numeric(crossprod(beta-beta_LS))
   
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
